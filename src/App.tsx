f2359c8a97e5d99f7e0f19f772930a41cbf19f97
ASHISH MOHANTY <ashishmohaty10@gmail.com>
2024-08-16 19:23:01 +0530

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import CatListing from "./components/catListing";
 import RandomeJokes from "./components/randomeJokes";
 import Randomeuser from "./components/randomeuser";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
